f46eeccb59b9bf2e599d57a34c0b76ffc4d424b4
Louis Mony <73167166+LouisMony@users.noreply.github.com>
2022-06-24 22:56:19 +0200

    WIP

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="navbar">
+    <span>PRONO APP</span>
+    <button>Log in</button>
+    <button>Sign up</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+}
+</script>
+<style lang="scss">
+
+.navbar{
+    position: absolute;
+    width: 100%;
+    height: 50px;
+    top: 0;
+    left: 0;
+    background: rgb(40, 40, 40);
+    display: flex;
+    align-items: center;
+    gap: 20px;
+    padding: 0 0 0 20px;
+
+    span{
+        color: white
+    }
+}
+
+</style>
+
